b6b8f4df4101f08998d5260fc7b42ef4f67f8739
jmskeoch <jmskeoch@yahoo.com>
2021-11-13 13:23:04 -0800

    Auto Routine 5 Implementation pt. 2

--- src/main/java/frc/robot/commands/autonomous/routines/AutoRoutine5.java
+++ src/main/java/frc/robot/commands/autonomous/routines/AutoRoutine5.java
@@ -1,2 +1,19 @@
-package frc.robot.commands.autonomous.routines;public class AutoRoutine5 {
+package frc.robot.commands.autonomous.routines;
+
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.commands.autonomous.components.AutoForwardDistance;
+import frc.robot.commands.autonomous.components.AutoIntakeOn;
+import frc.robot.subsystems.DrivetrainSubsystem;
+import frc.robot.subsystems.IntakeSubsystem;
+
+public class AutoRoutine5 extends SequentialCommandGroup {
+    public AutoRoutine5(DrivetrainSubsystem drivetrain, IntakeSubsystem intake) {
+        addRequirements(drivetrain);
+        addCommands(
+                new AutoIntakeOn(intake),
+                new AutoForwardDistance(drivetrain, 6),
+                new AutoForwardDistance(drivetrain, -4)
+        );
+
+    }
 }
