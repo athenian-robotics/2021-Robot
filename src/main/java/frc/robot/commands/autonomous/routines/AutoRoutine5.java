511466886adad3f14beb6cdb1dd96336a0d48873
jmskeoch <jmskeoch@yahoo.com>
2021-11-13 13:23:54 -0800

    Turn off intake during Auto Routine 5

--- src/main/java/frc/robot/commands/autonomous/routines/AutoRoutine5.java
+++ src/main/java/frc/robot/commands/autonomous/routines/AutoRoutine5.java
@@ -2,6 +2,7 @@ package frc.robot.commands.autonomous.routines;
 
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.commands.autonomous.components.AutoForwardDistance;
+import frc.robot.commands.autonomous.components.AutoIntakeOff;
 import frc.robot.commands.autonomous.components.AutoIntakeOn;
 import frc.robot.subsystems.DrivetrainSubsystem;
 import frc.robot.subsystems.IntakeSubsystem;
@@ -12,7 +13,8 @@ public class AutoRoutine5 extends SequentialCommandGroup {
         addCommands(
                 new AutoIntakeOn(intake),
                 new AutoForwardDistance(drivetrain, 6),
-                new AutoForwardDistance(drivetrain, -4)
+                new AutoForwardDistance(drivetrain, -4),
+                new AutoIntakeOff(intake)
         );
 
     }
